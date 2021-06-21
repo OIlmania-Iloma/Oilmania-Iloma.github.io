9d689ca44dc7e5156af2a51ae57e7e54370275b3
oilmania-iloma <oilmaniailoma@gmail.com>
2021-06-21 17:34:21 +0100

    Responsivity tesing branch

--- wrks.js
+++ wrks.js
@@ -1,14 +1,45 @@
-let navul = document.getElementsByTagName('ul')
-navul.classList.add('navbar-nav')
-let navItems = ['journals', 'membership', 'covid news', 'announcements'];
-for (let i=0; i<navItems.length; i++){
-    let a = document.createElement('a');
-    let txt = document.createTextNode(navItems[i]);
-    a.appendChild(txt);
-    a.classList.add('nav-link');
-    navL.appendChild(a);  
-};
+const menu = document.querySelector('.menu');
+const toggle = document.querySelector('.toggle')
+let toggleMenu = () => {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML= "<i class='fas fa-bars'></i>"
+    }else{
+        menu.classList.add("active")
+        toggle.querySelector("a").innerHTML ="<i class='fas fa-times'></i>"
+    }
+    
+}
+toggle.addEventListener("click", toggleMenu, false);
 
-setTimeout(() => {
-    alert('wrll')
-}, 1000);
+const items = document.querySelectorAll(".li");
+function toggleli(){
+    const items = document.querySelectorAll(".li");
+    if (this.classList.contains("submenu-active")){
+        this.classList.remove("submenu-active");
+    }else if (menu.querySelector(".submenu-active")){
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    }else{
+        this.classList.add("submenu-active");
+    }
+    }
+
+for (let item of items){
+    if (item.querySelector(".submenu")){
+        item.addEventListener('click', toggleli, false);
+        item.addEventListener('keypress', toggleli, false);
+
+
+    }
+}
+//close submenu
+function closeSubMenu (e){
+    let isClickInside = menu.contains(e.target);
+    if(!isClickInside && menu.querySelector('.subemenu-active')){
+        menu.querySelector('.submenu-active').
+        classList.remove('submenu-active')
+    }
+
+}
+document.addEventListener('click', closeSubMenu, false)
